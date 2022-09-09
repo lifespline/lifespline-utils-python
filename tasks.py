42bdb19268222b626f7450eb0b9e56c339831855
diogo <dipm@novonordisk.com>
2022-09-09 13:15:14 +0000

    dev: add docs tasks

--- tasks.py
+++ tasks.py
@@ -1,57 +1,50 @@
 """This is the project's task runner.
 """
 from invoke import task
+import os
 
 @task
-def ci(ctx, suite = '', test = 'all', host = '', database = '', port = '', user = '', password = '', lint = ''):
-    """Continuous integration tools: UT, lint.
-
-    `tasks.ci` is ran both by a human agent or by an automated agent. The human 
-    agent must provide the method's parameters without persisting them in the 
-    solution. The automated agent provides the parameters through the 
-    pipeline's variables.
-
-    # Args
-
-    + ctx (_type_): _description_
-    + suite (str, optional): Run unit tests. See examples below. Defaults to an empty string.
-    + test (str, optional): Run unit test. See examples below. Defaults 'all'.
-    + host (str, optional): _description_. Defaults to an empty string.
-    + database (str, optional): _description_. Defaults to an empty string.
-    + port (str, optional): _description_. Defaults to an empty string.
-    + user (str, optional): _description_. Defaults to an empty string.
-    + password (str, optional): _description_. Defaults to an empty string.
-    + lint (str, optional): _description_. Defaults to an empty string.
-
-    # Examples
-
-    Running unit tests:
-
-    ```python
-    >>> inv ci --suite empower \ 
-        --user <user> \ 
-        --password <password> \ 
-        --port <port> \ 
-        --host <host> \ 
-        --database <database>
-
-    >>> inv ci --suite empower \ 
-        --test invalid_user \ 
-        --user <user> \ 
-        --password <password>
-    ```
-    """
-
-    pass
-
-@task
-def docs(ctx):
+def docs(ctx, step='build'):
     """Generate documentation.
     """
-    cmds = [
-        "cd docs/sphinx",
-        "make html",
-        "cp -a _build/html/. ..",
-        "make clean",
-    ]
+
+    image_name = 'docs_debug_lifespline_demo_rst'
+    container_name = image_name
+    volume_root = os.getcwd()
+    volume_path = f'{volume_root}/docs/debug'
+    container_host = '0.0.0.0'
+    container_port = 8080
+
+    if step == 'debug-setup':
+        docker_build = f"docker build . -t {image_name}"
+        docker_run = f"""
+        docker run \
+            --rm -d \
+            --mount type=bind,source={volume_path},target=/usr/src/app \
+            -p {container_host}:{container_port}:{container_port} \
+            --name {container_name} \
+            {image_name} {container_port} {container_host}
+        """
+
+        cmds = [
+            'cd docs/debug',
+            docker_build,
+            docker_run,
+
+        ]
+    elif step == 'debug-clear-setup':
+        cmds = [
+            f'docker stop {container_name}',
+            f'docker rm {container_name}',
+            f'docker rmi {image_name}',
+        ]
+    elif step == 'build':
+        cmds = [
+            "cd docs",
+            "make html",
+            "cp -a _build/html/. debug/static/",
+            "make clean",
+            "echo && echo Your static documentation pages can be found at \'docs/debug/static\' && echo",
+        ]
+    
     ctx.run(';'.join(cmds))
